cb461e4042c78bcf53fa5277db347731901ced25
Toufiq <burningraven06@gmail.com>
2018-10-30 21:14:21 +0600

    make delete

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const branchList = document.querySelector("#branch-list");
 const addBranchForm = document.querySelector("#add-branch-form");
 
-function renderBranchElem(doc){
+function renderBranchElem(doc, docId){
   let liElem = document.createElement("li");
   let city = document.createElement("span");
   let name = document.createElement("span");
+  let delBtn = document.createElement("button");
   
-  liElem.setAttribute("data-id", doc.id);
+  liElem.setAttribute("data-id", docId);
+  liElem.classList.add("list-group-item");
+  
   name.textContent = doc.name;
   city.textContent = doc.city;
 
@@ -14,7 +17,25 @@ function renderBranchElem(doc){
   liElem.appendChild(document.createTextNode(" : "));
   liElem.appendChild(city);
 
+  delBtn.classList.add("btn");
+  delBtn.classList.add("btn-danger");
+  delBtn.innerHTML = " X ";
+  delBtn.classList.add("float-right");
+  delBtn.addEventListener("click", (e) => handleDeleteBranch(e));
+  delBtn.setAttribute("data-id", docId);
+
+  liElem.appendChild(delBtn);
+
   branchList.appendChild(liElem);
+}
+
+
+function handleDeleteBranch(e){
+  e.stopPropagation();
+  let docId = e.srcElement.getAttribute("data-id")
+  let docIdP = e.srcElement.parentElement.attributes["data-id"].nodeValue;
+  
+  firebaseDB.collection(AppKeys.dbCollectionName).doc(docIdP).delete();
 }
 
 function handleAddBranch(e){
@@ -28,7 +49,7 @@ function handleAddBranch(e){
   
   addBranchForm.name.value = "";
   addBranchForm.city.value = "";
-  
+
 }
 
 addBranchForm.addEventListener("submit" , (e) => handleAddBranch(e));
@@ -39,7 +60,7 @@ let getAllBranches = async function(){
   var branches = await firebaseDB.collection(AppKeys.dbCollectionName).get();
   var branchDocs = branches.docs;
 
-  branchDocs.forEach( doc => renderBranchElem(doc.data()) );
+  branchDocs.forEach( doc => renderBranchElem(doc.data() , doc.id) );
 }
 
 getAllBranches();
